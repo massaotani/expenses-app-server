211c5ba6ed328245e930cd703fd781da6e2fba0b
Massao <tani22massao@gmail.com>
2026-08-19 14:02:31 +0900

    Fix on user get methods

--- server/src/main/java/com/expensesapp/server/dto/UserResponseDto.java
+++ server/src/main/java/com/expensesapp/server/dto/UserResponseDto.java
@@ -1,24 +1,34 @@
 package com.expensesapp.server.dto;
 
-import com.expensesapp.server.model.User;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record UserResponseDto(
-    UUID id,
-    String name,
-    BigDecimal monthlyIncome,
-    BigDecimal investmentPot,
-    BigDecimal monthlyExpenses
-) {
+import com.expensesapp.server.model.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+
+    private UUID id;
+    private String name;
+    private BigDecimal monthlyIncome;
+    private BigDecimal investmentPot;
+    private BigDecimal monthlyExpenses;
+
     public static UserResponseDto fromEntity(User user) {
-        return new UserResponseDto(
-            user.getId(),
-            user.getName(),
-            user.getMonthlyIncome(),
-            user.getInvestmentPot(),
-            user.getMonthlyExpenses()
-        );
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .monthlyIncome(user.getMonthlyIncome())
+                .investmentPot(user.getInvestmentPot())
+                .monthlyExpenses(user.getMonthlyExpenses())
+                .build();
     }
 }

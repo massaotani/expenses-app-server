88699407a667d5491a21398b0130af1599736c82
Massao <tani22massao@gmail.com>
2026-09-02 18:29:49 +0900

    Health check limited to only Render server.

--- server/src/main/java/com/expensesapp/server/controller/HealthController.java
+++ server/src/main/java/com/expensesapp/server/controller/HealthController.java
@@ -1,10 +1,6 @@
 package com.expensesapp.server.controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/health")
 public class HealthController {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @GetMapping
-    public ResponseEntity<Map<String, String>> healthCheck() {
-        jdbcTemplate.execute("SELECT 1"); // Keeps DB connection pool active
-        return ResponseEntity.ok(Map.of("status", "UP"));
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("UP");
     }
 }

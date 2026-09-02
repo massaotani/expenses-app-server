dfacecad0e7fc5343745dd301502fa611f2e12a3
Massao <tani22massao@gmail.com>
2026-09-02 17:45:43 +0900

    Health Monitor added.

--- server/src/main/java/com/expensesapp/server/controller/HealthController.java
+++ server/src/main/java/com/expensesapp/server/controller/HealthController.java
@@ -2,7 +2,9 @@ package com.expensesapp.server.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/health")
 public class HealthController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping
     public ResponseEntity<Map<String, String>> healthCheck() {
+        jdbcTemplate.execute("SELECT 1"); // Keeps DB connection pool active
         return ResponseEntity.ok(Map.of("status", "UP"));
     }
 }

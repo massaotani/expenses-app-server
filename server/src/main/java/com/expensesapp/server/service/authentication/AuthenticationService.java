441606add696f6b26c70a09ac990da852f1e90d5
Massao <tani22massao@gmail.com>
2026-09-04 18:20:57 +0900

    Password Change core logic applied. v2

--- server/src/main/java/com/expensesapp/server/service/authentication/AuthenticationService.java
+++ server/src/main/java/com/expensesapp/server/service/authentication/AuthenticationService.java
@@ -1,9 +1,11 @@
 package com.expensesapp.server.service.authentication;
 
 import com.expensesapp.server.dto.AuthenticationResponse;
+import com.expensesapp.server.dto.ChangePasswordRequest;
 import com.expensesapp.server.dto.LoginRequest;
 import com.expensesapp.server.dto.RegisterRequest;
 import com.expensesapp.server.dto.TokenRefreshRequest;
+import com.expensesapp.server.model.AuthUser;
 
 public interface AuthenticationService {
 
@@ -12,4 +14,6 @@ public interface AuthenticationService {
     AuthenticationResponse authenticate(LoginRequest request);
 
     AuthenticationResponse refreshToken(TokenRefreshRequest request);
+
+    void changePassword(AuthUser authUser, ChangePasswordRequest request);
 }
